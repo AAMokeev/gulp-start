5339a5733f3c7e9a1193cb49814449188a046009
Artem Mokeev <artem-mokeev@mail.ru>
2023-07-20 14:46:01 +0300

    Change svg_sprite.js task
    * created 2 functions: svg_sprite_mono for single color svg,
      svg_sprite_multi for multicolor svg
    * changed template: _template-svg.scss
    * added .gitkeep files for src/svg/mono and src/svg/multi folders

--- tasks/svg_sprite.js
+++ tasks/svg_sprite.js
@@ -1,35 +1,83 @@
-const { src, dest } = require('gulp');
+const { src, dest, parallel } = require('gulp');
 
 const svgmin = require('gulp-svgmin');
 const sprite = require('gulp-svg-sprite');
-const cheerio = require('gulp-cheerio');
-const replace = require('gulp-replace');
 
-module.exports = function svg_sprite() {
-	return src(['src/svg/**/*.svg', '!src/svg/sprites/**/*.svg'])
-		.pipe(cheerio({
-			run: function ($) {
-				$('[style]').removeAttr('style');
-				$('[stroke]').removeAttr('stroke');
-			},
-			parserOptions: { xmlMode: true }
-		}))
-		.pipe(replace('&gt;', '>'))
+const spriteMono = function svg_sprite_mono() {
+	return src(['src/svg/mono/*.svg', '!src/svg/sprites/**/*.svg'])
 		.pipe(svgmin())
 		.pipe(sprite({
 			mode: {
 				symbol: {
-					sprite: '../sprites/sprite.svg',
+					sprite: '../sprites/sprite-mono.svg',
 					render: {
 						scss: {
-							dest:'../../scss/svg/sprite.scss',
+							dest:'../../scss/svg/sprite-mono.scss',
 							template:'src/scss/template/_template-svg.scss'
-						}
-					}
-				}	
-			}
+						},
+					},
+				},	
+			},
+			shape: {
+				transform: [
+				  {
+					svgo: {
+						plugins: [
+							{
+								name: "removeAttrs",
+								params: {
+								attrs: ['stroke.*', 'fill', 'class', 'data-name']
+								}
+							},
+						],
+					},
+				},
+				],
+			  },
 		}))
 		.pipe(dest('src/svg'))
-		.pipe(src(['src/svg/**/*.svg']))
-		.pipe(dest('build/svg'))
 }
+
+const spriteMulti = function svg_sprite_multi() {
+	return src(['src/svg/multi/**/*.svg', '!src/svg/sprites/**/*.svg'])
+		.pipe(svgmin())
+		.pipe(sprite({
+			mode: {
+				symbol: {
+					sprite: '../sprites/sprite-multi.svg',
+					render: {
+						scss: {
+							dest:'../../scss/svg/sprite-multi.scss',
+							template:'src/scss/template/_template-svg.scss'
+						},
+					},
+				},	
+			},
+			shape: {
+				transform: [
+				  {
+					svgo: {
+						plugins: [
+							{
+								name: "removeAttrs",
+								params: {
+								attrs: ['class', 'data-name']
+								}
+							},
+							{
+								name: 'removeUselessStrokeAndFill',
+								params: {
+									stroke: removeStroke = false,
+								}
+
+							},
+						],
+					},
+				},
+				],
+			  },
+		}))
+		.pipe(dest('src/svg'))
+}
+
+module.exports = parallel(spriteMono, spriteMulti)
